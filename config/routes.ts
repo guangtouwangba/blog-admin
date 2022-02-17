04b184ced9c59801b642428af6c89b87fb0e24e5
Siqiu Chen <819110812@qq.com>
2022-02-17 14:55:02 +0800

    添加个人中心

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,4 @@
-﻿export default [
+export default [
   {
     path: '/user',
     layout: false,
@@ -51,6 +51,12 @@
   {
     path: '/',
     redirect: '/welcome',
+  },
+  {
+    name: '个人中心',
+    icon: 'smile',
+    path: '/accountcenter',
+    component: './AccountCenter',
   },
   {
     component: './404',
