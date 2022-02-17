589f9729b3f543ed61f810247679148bfd208710
Siqiu Chen <819110812@qq.com>
2022-02-17 13:45:50 +0800

    去除水印

--- src/app.tsx
+++ src/app.tsx
@@ -55,9 +55,6 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
   return {
     rightContentRender: () => <RightContent />,
     disableContentMargin: false,
-    waterMarkProps: {
-      content: initialState?.currentUser?.name,
-    },
     footerRender: () => <Footer />,
     onPageChange: () => {
       const { location } = history;
